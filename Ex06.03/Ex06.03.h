0b1c26f8a3717b2b376519600db7d8f6dfbfc380
YishaiDredzen <88730903+YishaiDredzen@users.noreply.github.com>
2021-08-30 15:01:39 +0300

    Update Ex06.03.h

--- Ex06.03/Ex06.03.h
+++ Ex06.03/Ex06.03.h
@@ -2,7 +2,7 @@
 #define _EX06_03_H_
 
 /**
- * @brief Prints the memory binary values of what is sent as a perameter.
+ * @brief Prints the memory binary values of what is sent as a parameter.
  * @param value - the address of the variable to print
  * @param size - size of the variable in memory (number of bytes it takes up)
 */
